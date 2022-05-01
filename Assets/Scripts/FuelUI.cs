c3d2f25c52becf39fe1df6a2d61c6e46cd2a28f9
Naya <naya.al.tahan@lego.com>
2022-05-01 09:59:41 +0200

    fuel

--- Assets/Scripts/FuelUI.cs
+++ Assets/Scripts/FuelUI.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FuelUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public Image fuelBar;
+    public PlayerFuelController fuelController;
+ 
     void Update()
     {
-        
+        fuelBar.fillAmount = fuelController.currentFuel / fuelController.fullFuel;
     }
+    
 }
