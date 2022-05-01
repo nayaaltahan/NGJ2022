db8601c6a9a1647013239b30ca9f39c925413d20
Naya <naya.al.tahan@lego.com>
2022-05-01 12:14:59 +0200

    fix score display

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -16,6 +16,6 @@ public class ScoreUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        scoreDisplay.SetText("Score: " + LevelManager.Instance.currentScore);
+        scoreDisplay.SetText(Mathf.RoundToInt(LevelManager.Instance.currentScore).ToString());
     }
 }
